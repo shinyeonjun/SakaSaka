832e62ab3f83cef4b72c000e8f76909bd878915b
Shin Yeonjun <162365636+shinyeonjun@users.noreply.github.com>
2026-09-18 00:34:07 +0900

    feat: enable desktop workflow styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { AppProvider } from "./store";
 import "./styles.css";
 import "./runtimeDiagnostics.css";
 import "./desktopV2.css";
+import "./desktopV2Screens.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
